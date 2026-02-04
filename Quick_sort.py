c55c4ff902c9f7afdb0c9be848bc1b1650c13317
RiteshBadgujar <riteshbadgujar0@gmail.com>
2026-02-04 23:27:41 +0530

    [ADDED]:Ritesh -:The Quick sort program.

---
+++ Quick_sort.py
@@ -0,0 +1,25 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[0]
+    left = []
+    right = []
+
+    for i in range(1, len(arr)):
+        if arr[i] <= pivot:
+            left.append(arr[i])
+        else:
+            right.append(arr[i])
+
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
+
+n = int(input("Enter size: "))
+arr = []
+
+for i in range(n):
+    arr.append(int(input("Enter element: ")))
+
+sorted_arr = quick_sort(arr)
+print("Sorted:", sorted_arr)
