1569cc57eebbf47f4636a440747a6a1739535d3e
RiteshBadgujar <riteshbadgujar0@gmail.com>
2026-02-17 23:06:19 +0530

    [ADDED]:Ritesh -:The fibonaccy series using input program.

---
+++ Fibonaccy_dp.py
@@ -0,0 +1,14 @@
+n = int(input("Enter number: "))
+
+if n <= 1:
+    print(n)
+else:
+    a = 0
+    b = 1
+
+    for i in range(2, n + 1):
+        c = a + b
+        a = b
+        b = c
+
+    print(b)
